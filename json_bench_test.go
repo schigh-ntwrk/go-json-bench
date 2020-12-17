f84ffd61a206618a0557c4e944ab7718b4630ad0
schigh-ntwrk <steve@thentwrk.com>
2020-12-17 14:58:49 -0500

    added results

--- json_bench_test.go
+++ json_bench_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 )
@@ -14,7 +16,7 @@ type JSON struct {
 	Type     string `json:"type"`
 }
 
-var (
+const (
 	j = `{
    "name":"id",
    "type":"UUID",
@@ -35,6 +37,65 @@ func unmarshal() error {
 func decode() error {
 	var target JSON
 	return json.NewDecoder(strings.NewReader(j)).Decode(&target)
+}
+
+func BenchmarkHTTPUnmarshal(b *testing.B) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		data, readErr := ioutil.ReadAll(r.Body)
+		if readErr != nil {
+			return
+		}
+		_ = r.Body.Close()
+
+		var target JSON
+		if jsonErr := json.Unmarshal(data, &target); jsonErr != nil {
+			return
+		}
+	}))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		req, err := http.NewRequest(http.MethodPost, server.URL, strings.NewReader(j))
+		if err != nil {
+			b.Error(err)
+		}
+
+		_, respErr := http.DefaultClient.Do(req)
+		if respErr != nil {
+			b.Error(respErr)
+		}
+	}
+	b.StopTimer()
+}
+
+func BenchmarkHTTPJSONDecode(b *testing.B) {
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		var target JSON
+		if err := json.NewDecoder(r.Body).Decode(&target); err != nil {
+			b.Error(err)
+		}
+		_ = r.Body.Close()
+	}))
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		req, err := http.NewRequest(http.MethodPost, server.URL, strings.NewReader(j))
+		if err != nil {
+			b.Error(err)
+		}
+
+		_, respErr := http.DefaultClient.Do(req)
+		if respErr != nil {
+			b.Error(respErr)
+		}
+	}
+	b.StopTimer()
 }
 
 func BenchmarkJSONUnmarshal(b *testing.B) {
